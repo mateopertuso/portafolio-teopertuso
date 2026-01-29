31672be8d894bd165d201a322460492f6b6cf837
mateopertuso <mateopertuso.dev@gmail.com>
2026-01-29 12:53:49 -0300

    feat(contact): improve form validation and disabled submit UX

--- portafolio-teopertuso/src/app/pages/contact/contact.ts
+++ portafolio-teopertuso/src/app/pages/contact/contact.ts
@@ -1,11 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.css',
 })
 export class Contact {
+  form = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  };
 
+  loading = false;
+  success = false;
+  error = false;
+
+  sendEmail() {
+    this.loading = true;
+    this.success = false;
+    this.error = false;
+    emailjs
+      .send(
+        'service_zie3fbt',
+        'template_lcatdui',
+        {
+          from_name: this.form.name,
+          from_email: this.form.email,
+          subject: this.form.subject,
+          message: this.form.message,
+        },
+        'ou1MCTn0J4JtBwg-G',
+      )
+      .then(() => {
+        this.form = { name: '', email: '', subject: '', message: '' };
+        this.success = true;
+      })
+      .catch((error) => {
+        console.error('Email error:', error);
+        this.error = true;
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  }
 }
